22e69d19ae0f8bf5bd1571298d86d7a57d659b3f
jhonsonlee <jhonsonlee10@gmail.com>
2019-11-28 22:12:09 +0700

    add batchnorm for input state

--- src/model.py
+++ src/model.py
@@ -14,6 +14,7 @@ class Actor(nn.Module):
     def __init__(self, state_size, action_size, seed, fc1_units=300, fc2_units=200):
         super(Actor, self).__init__()
         self.seed = torch.manual_seed(seed)
+        self.bn0 = nn.BatchNorm1d(state_size)
         self.fc1 = nn.Linear(state_size, fc1_units)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
         self.fc3 = nn.Linear(fc2_units, action_size)
@@ -25,6 +26,7 @@ class Actor(nn.Module):
         self.fc3.weight.data.uniform_(-3e-3, 3e-3)
         
     def forward(self, state):
+        state = self.bn0(state)
         x = F.relu(self.fc1(state))
         x = F.relu(self.fc2(x))
         return F.tanh(self.fc3(x))
@@ -45,6 +47,7 @@ class Critic(nn.Module):
         """
         super(Critic, self).__init__()
         self.seed = torch.manual_seed(seed)
+        self.bn0 = nn.BatchNorm1d(state_size)
         self.fcs1 = nn.Linear(state_size, fcs1_units)
         self.fc2 = nn.Linear(fcs1_units+action_size, fc2_units)
         self.fc3 = nn.Linear(fc2_units, fc3_units)
@@ -59,6 +62,7 @@ class Critic(nn.Module):
 
     def forward(self, state, action):
         """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
+        state = self.bn0(state)
         xs = F.leaky_relu(self.fcs1(state))
         x = torch.cat((xs, action), dim=1)
         x = F.leaky_relu(self.fc2(x))
